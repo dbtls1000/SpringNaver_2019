b603809007e531f1bbf6a598c5343c500a68042f
dbtls1000 <dbsqhtjs@naver.com>
2019-08-07 11:28:48 +0900

    first

---
+++ src/main/java/com/biz/naver/config/NaverSecret_Sample.java
@@ -0,0 +1,15 @@
+package com.biz.naver.config;
+
+/*
+ * 네이버 오픈 API 사용등록을 수행한 후
+ * 본인의 CLIENT ID 와 SECRET을 발급받아
+ * 아래 String변수에 값을 복사한 후
+ * 이 class를 NaverSecret 이름으로 변경한 후 
+ * 프로젝트를 실행하시오
+ */
+public class NaverSecret_Sample {
+	
+	public final static String NaverClientId = "YOUR NAVER CLIENT ID";
+	public final static String NaverClientSecret = "YOUR NAVER SECRET";
+	
+}
